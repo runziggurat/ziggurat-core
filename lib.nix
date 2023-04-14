0b59c9cd51988ae7feaaf15c8ea28423a5d45939
zeapoz <zeapo@pm.me>
2023-04-14 12:16:52 +0200

    fix(nix): interpret scripts with `runtimeShell`

--- lib.nix
+++ lib.nix
@@ -6,7 +6,7 @@ let
     echo "Running \"${command}\"..."
     ${command} \
     && echo -e "\033[0;32mSucceeded!\033[0m" \
-    || echo -e "\033[0;31mFailed!\033[0m"
+    || (s=$?; echo -e "\033[0;31mFailed!\033[0m"; exit $s)
   '';
 
   # Wrapper function around `mkCiScriptsBin` and `mkCiScriptBin`.
@@ -15,12 +15,12 @@ let
 
   # Writes an executable script for each entry in an attribute set.
   mkCiScriptsBin = s: attrsets.mapAttrsToList
-    (name: command: (pkgs.writeScriptBin "ci-${name}"
+    (name: command: (pkgs.writeShellScriptBin "ci-${name}"
       (ciScriptTemplate { inherit name command; })))
     s;
 
   # Writes an executable script that will run all entries in an attribute set.
-  mkCiAllScriptBin = s: pkgs.writeScriptBin "ci-all"
+  mkCiAllScriptBin = s: pkgs.writeShellScriptBin "ci-all"
     (strings.concatStringsSep "\necho\n"
       (attrsets.mapAttrsToList
         (name: command: (ciScriptTemplate { inherit name command; }))
